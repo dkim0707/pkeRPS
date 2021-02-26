309c21c3ec3769c28c1b722511f2186589e83161
Daniel Kim <daniel.y.kim1995@gmail.com>
2021-02-26 15:08:27 +0900

    modified the button to run a round and figured out how to use DOM manipulation to change HTML

--- RPS.js
+++ RPS.js
@@ -1,19 +1,34 @@
 function computerPlay() {
     let comp = Math.floor(Math.random() * Math.floor(3));
     if (comp == 0){
-        return "grass";
+        return "bulb";
     } else if (comp == 1) {
-        return "water";
+        return "squirtle";
     } else if (comp == 2) {
-        return "fire";
+        return "charmander";
     }
 }
 
-function playerPlay() {
-    return document.getElementById;
+function rivalTextChoose() {
+    let rivalText = document.getElementById("rivalText");
+    rivalText.textContent = "I choose you...!"
 }
 
 function round() {
+    let computerSelection = computerPlay();
+    let playerSelection = document.getElementsByName("playerPoke");
+    let image = document.getElementById("rivalPoke");
+    image.src = computerSelection + ".png";
+    var playerValue;
+    for(var i = 0; i < playerSelection.length; i++){
+        if(playerSelection[i].checked){
+            playerValue = playerSelection[i].value;
+        }
+    }
+
+    if (computerSelection == playerValue) {
+        document.getElementById("result").textContent = "Tie";
+    }
     
 }
 
